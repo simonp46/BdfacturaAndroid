085b01a798b926317a8018fc15dcb68af8b04a8a
Simon Padron <89358707+simonp46@users.noreply.github.com>
2021-09-07 13:15:00 -0500

    git commit -m "Version 3.1"

--- app/src/main/java/com/example/bdfactura/Registrar.java
+++ app/src/main/java/com/example/bdfactura/Registrar.java
@@ -3,12 +3,24 @@ package com.example.bdfactura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Registrar extends AppCompatActivity {
+    //instanciar objetos de activity_registrar.xml
+    EditText cedula, nombres, contras;
+    TextView iniciarsesionr;
+    Button registrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+        cedula = findViewById(R.id.etCedR);
+        nombres = findViewById(R.id.etNom);
+        contras = findViewById(R.id.etContraR);
+        iniciarsesionr = findViewById(R.id.tvIniR);
+        registrar = findViewById(R.id.btnreg);
     }
 }
